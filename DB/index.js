efa5951aef4c908b0be0e9cf633bfe7fc8fc122c
Debanjan Barman <debanjanbarman123456789@gmail.com>
2024-06-23 21:13:04 +0530

    connecting HOSTED PostgreSQL

--- DB/index.js
+++ DB/index.js
@@ -1,8 +1,17 @@
 const {Pool} = require('pg');
-exports.pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'mcalab',
-    password: 'pass1234',
-    port: 5432,
-})
+const dotenv = require("dotenv");
+dotenv.config({path: "./config.env"});
+
+// Hosted DB
+let connectionString = "postgres://postgres.fjghcpzuknouvfnkkgiy:[YOUR-PASSWORD]@aws-0-us-east-1.pooler.supabase.com:6543/postgres";
+connectionString = connectionString.replace("[YOUR-PASSWORD]", process.env.DATABASE_PASSWORD);
+// Local DB
+// exports.pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'mcalab',
+//     password: 'pass1234',
+//     port: 5432,
+// })
+
+exports.pool = new Pool({connectionString});
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 const gameRouter = require("./routes/gameRoutes");
 const performanceRouter = require("./routes/performanceRoutes");
-
+const initDB = require("./DB/initializeDB");
 const app = express();
 const PORT = 3000;
 dotenv.config({path: "./config.env"});
@@ -18,6 +18,15 @@ app.options("/", cors());
 app.use("/api/users", userRouter);
 app.use("/api/games", gameRouter);
 app.use("/api/performance", performanceRouter);
+
+(async () => {
+    const db = await initDB.initializeDB();
+    if (db) {
+        console.log("DB connected successfully");
+    } else {
+        console.log(db);
+    }
+})();
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
