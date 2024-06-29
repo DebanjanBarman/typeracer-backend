664af087a511fb71e276b60cfb93508f7ef6bdb3
sourab6058 <sourabkumar1236@gmail.com>
2024-06-29 09:40:01 +0530

    Sourab's changes

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const dotenv = require("dotenv");
-const express = require('express');
+const express = require("express");
 const cors = require("cors");
 const userRouter = require("./routes/userRoutes");
 const gameRouter = require("./routes/gameRoutes");
 const performanceRouter = require("./routes/performanceRoutes");
-const initDB = require("./DB/initializeDB");
+const eventRouter = require("./routes/eventRoutes");
+const adminRouter = require("./routes/adminRoutes");
+
 const app = express();
 const PORT = 3000;
-dotenv.config({path: "./config.env"});
+dotenv.config({ path: "./config.env" });
 
-app.use(express.json({limit: "100kb"}));
-app.use(cors({origin: "*"}));
+app.use(express.json({ limit: "100kb" }));
+app.use(cors({ origin: "*", exposedHeaders: "Content-Range" }));
 
 app.options("/", cors());
-
 
 app.use("/api/users", userRouter);
 app.use("/api/games", gameRouter);
 app.use("/api/performance", performanceRouter);
+app.use("/api/event", eventRouter);
+app.use("/api/admin", adminRouter);
 
-(async () => {
-    const db = await initDB.initializeDB();
-    if (db) {
-        console.log("DB connected successfully");
-    } else {
-        console.log(db);
-    }
-})();
+app.get("/", (req, res) => {
+  res.send("working");
+});
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}`);
-})
+  console.log(`App running on port ${PORT}`);
+});
